f56ee52ee7f7ab91309cb8146b582df15b1b72ee
Nicholas LeFevre <nlefevre@widener.edu>
2021-09-20 18:29:55 -0400

    starting cli app

---
+++ Java-Practice/src/com/revature/main/RollingDice.java
@@ -0,0 +1,16 @@
+package com.revature.main;
+
+import java.util.Random;
+
+public class RollingDice {
+
+	public static void main(String[] args) {
+		
+		Random rand = new Random();
+		int x = rand.nextInt(6) + 1;
+		
+		System.out.println("You rolled a: " + x);
+
+	}
+
+}
